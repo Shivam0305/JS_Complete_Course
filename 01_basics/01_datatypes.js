3520adb5474fe8bb36280ba0dd4ccbd47addf9d0
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-03-13 06:32:17 +0000

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // Treat all js code as a newer version
+console.log(3+3)
+console.log("Shivam")
+// Num
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+// Object
+console.log(typeof null)
