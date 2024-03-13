767d8c31502ee7d7f14e8a19221ac5853b5e49f4
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-03-13 06:14:22 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accoundId=144553
+
+let accountEmail="shivam@gmail.com"
+
+let accoundPassword="12345"
+
+accountCity="patna"
+
+accountEmail="raksha@gmail.com"
+console.log(accountEmail);
+console.table([accoundId,accoundPassword,accountCity,accountEmail])
