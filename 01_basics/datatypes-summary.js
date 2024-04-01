9ebd31c1a2d75781bc6c6e0d4159ac84b91298cd
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-04-01 19:09:22 +0000

    datatypes Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+//Premitive
+// 7 types: string,number,boolean,null,undefined,symbol,BigInt
+const score=100;
+const scorevalue=100.3;
+const isloggedIn=false;
+
+const outsideTemp=null;
+
+let userEmail;
+
+
+// datatypes is symbol
+const id= Symbol('123')
+const anotherId=Symbol('123')
+
+// console.log(id===anotherId)
+
+// bigint datatypes
+const bigNumber=23453345335558474n
+
+
+//Reference or Non Primitive
+// 3 types: Array,objects,Function
+
+
+// object datatypes
+const heros=["Bhagat Singh","Chandrashekhar Azad","APJ Abdul kalam"]
+
+// object datatypes
+let myobj = {
+    Name :"shivam",
+    userEmail :"shivam123@gmail.com",
+
+}
+// function
+// datatypes fuction
+// const myFunction=function(){
+//     console.log("Hello World");
+// }
+console.log(typeof(id));
