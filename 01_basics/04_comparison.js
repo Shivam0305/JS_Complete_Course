254988da799a3e991ecb3bc8a850892ad68773d6
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-04-01 11:51:41 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+
+// both case is true
+// console.log("2">1);
+// console.log("02">1);
+
+// only last case is true
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// in every case output is false
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+// in this data type also check
+console.log("2"===2);
