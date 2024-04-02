faf34ecaae47ad1166dc62bc5c8c17467f628898
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-04-02 19:23:24 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,26 @@
+const name="shivam"
+const repocount=50
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+const gamename=new String ('shivam-hc')
+console.log(gamename[0]);
+console.log(gamename.__Proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('i'));
+
+const newString=gamename.substring(0,4)
+console.log(newString);
+
+const anotherString=gamename.slice(-8,4)
+console.log(anotherString);
+
+const newstringone="  shivam "
+console.log(newstringone.trim());
+console.log(newstringone);
+
+const url="https:/shivam.com/shivam %20 Jha"
+console.log(url.replace ('%20','-'));
+console.log(url.includes('sunday'));
+console.log(gamename.split('_'));
