bfd50107edb15e4f34cc0482186e931078e84663
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-04-05 05:52:27 +0000

    math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score=400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+//console.log(balance.toString());
+
+//console.log(balance.toFixed(2));
+//console.log(balance.toFixed(1));
+
+//const otherNumber=123.8966
+//console.log(otherNumber.toPrecision(5));
+
+const hundred=1000000
+//console.log(hundred.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.abs(4));
+// console.log(Math.round(6.453));
+// console.log(Math.ceil(5.456));
+// console.log(Math.floor(4.4));
+// console.log(Math.sqrt(4));
+// console.log(Math.max(3,6,7,5));
+// console.log(Math.min(2,4,3,5,6));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=1
+const max=6
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
