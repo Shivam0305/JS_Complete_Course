64cffbbd9733ecec18d153db2a9670dca03ec41c
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-04-06 18:11:05 +0000

    JSON Talk

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,27 @@
+//Json formatter
+
+const Course = {
+    CourseName:"js in hindi",
+    Price:"1999",
+    courseInstructor:"shivam"
+}
+
+// Course.courseInstructor
+
+const {courseInstructor:Instructor}=Course
+console.log(courseInstructor);
+console.log(Instructor);
+
+// {
+//     name:"shivam",
+//     coursename:"js_in_hindi",
+//     price:"free"
+
+// }
+[
+    {},
+
+    {},
+
+    {}
+]
