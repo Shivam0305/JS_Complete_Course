198563901954e658d8c3003b2bedc25728efcca1
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-07-10 19:05:02 +0000

    prototype complete

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,39 @@
+let nyName = "hitesh     "
+let mychannel ="chai    "
+
+console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+// Object.prototype.hitesh +=
+
+__proto__: TEachingSupport
+Teacher.__proto__ = User
+
+// modern syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
+let another__Uder = "ChaiAurCode   ";
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`${this.name}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength(){
+    "hitesh", trueLength();
+    "iceTea".trueLength();
+}
+
+
