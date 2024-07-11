875bc639df907b01c6fce4ca8d95612d4175e6c9
SHIVAM KUMAR JHA <112024350+Shivam0305@users.noreply.github.com>
2024-07-11 19:54:07 +0000

    Instance in Js

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,32 @@
+class User{
+    constructor(username){
+        this.username= username
+    }
+
+    logMe(){
+        console.log(`USERNAME IS ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email=email;
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was addes by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai","chai@gmail.com", "123")
+
+chai.addCourse
+
+const masalaChai = new User("masala chai")
+
+masalaChai.logMe()
+
+console.log(chai === masalaChai);
+console.log(chai instanceof User);
